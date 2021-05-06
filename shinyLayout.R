685a691b06eb8c76c9673a0ef964c38779d5a9dc
Jenny Yu <jennyyu@upei.ca>
2021-05-06 19:23:09 +0000

    Nothing changed

--- shinyLayout.R
+++ shinyLayout.R
@@ -51,10 +51,8 @@ ui <- fixedPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-}
+  }
 
 # Run the application 
-
 shinyApp(ui = ui, server = server)
 
